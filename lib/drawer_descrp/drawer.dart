6ff11465c8a64cb17614d18c9d53bb0b57d1f383
Shreezanaaryal <be2015se466@gces.edu.np>
2020-01-19 17:04:47 +0545

    Drawer and list page working

--- lib/drawer_descrp/drawer.dart
+++ lib/drawer_descrp/drawer.dart
@@ -13,66 +13,58 @@ class CurvedDrawer extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Drawer Demo"),
-      ),
-      drawer: SafeArea(
-        child:ClipPath(
+    return Container(
+      child: SafeArea(
+        child: ClipPath(
           clipper: RoundClipper(),
           child: Drawer(
-            child:Container(
+            child: Container(
               color: Colors.lightBlue,
               child: ListView(
-              padding: EdgeInsets.all(20.0),
-              children: <Widget>[
-                Padding(padding: EdgeInsets.fromLTRB(0, 0, 200, 30),
-                        child: Icon(Icons.dehaze,size: 25,color: Colors.white),
-                      
-                        ),
-                Column(
-                  children: listViewData
-                  .map((data) => ListTile(
-                        title: Text(data,style: TextStyle(fontSize: 20,color: Colors.white)),
-                        onTap: (){
-                                      // Update the state of the app
-                            // ...
-                            // Then close the drawer
-                            Navigator.pop(context);
-                      },
-                      ))
-
-                  .toList(),
-                )
-              ],
-              
+                padding: EdgeInsets.all(20.0),
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 200, 30),
+                    child: Icon(Icons.dehaze, size: 25, color: Colors.white),
+                  ),
+                  Column(
+                    children: listViewData
+                        .map((data) => ListTile(
+                              title: Text(data,
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.white)),
+                              onTap: () {
+                                // Update the state of the app
+                                // ...
+                                // Then close the drawer
+                                Navigator.pop(context);
+                              },
+                            ))
+                        .toList(),
+                  )
+                ],
+              ),
             ),
-            ),
-          
+          ),
         ),
-        ),
-      ),
-      body: Center(
-        child: Text('Main Body'),
       ),
     );
   }
 }
 
 class RoundClipper extends CustomClipper<Path> {
- @override
+  @override
   Path getClip(Size size) {
     Path path = Path();
     path.lineTo(0, size.height);
-    path.lineTo(size.width/2, size.height);
-    path.quadraticBezierTo(
-    size.height / 2, size.width, size.width/2,0);
+    path.lineTo(size.width / 2, size.height);
+    path.quadraticBezierTo(size.height / 2, size.width, size.width / 2, 0);
 
-    path.lineTo(size.width/2, 0);
+    path.lineTo(size.width / 2, 0);
     path.close();
     return path;
   }
-  
+
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) {
     return true;
