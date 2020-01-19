b8d0ef8fd101ac7ad89f1cc3d57f9d789fd9db07
Shreezanaaryal <be2015se466@gces.edu.np>
2020-01-19 12:35:41 +0545

    seventh commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'My Homefgdfgfdgdgd sssssssssssssssssssssssssssssssssssPage'),
+      home: MyHomePage(title: 'My Home Page'),
     );
   }
 }
