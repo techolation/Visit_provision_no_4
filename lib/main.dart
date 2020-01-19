3312cda09c63c5f1f002f0b3a09d61542618e8be
Shreezanaaryal <be2015se466@gces.edu.np>
2020-01-19 10:44:30 +0545

    foruth commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Srijana Aryal',
+      title: 'Srijana Aryalssssssssssssssssssssssss',
       theme: ThemeData(
         // This is the theme of your application.
         //
